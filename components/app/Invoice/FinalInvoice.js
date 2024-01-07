382bbceda865549b63eed49fa136e6c2ffc42752
Anil Karwa <anilkarwa3@gmail.com>
2024-01-07 17:15:49 +0530

    financial year fix

--- components/app/Invoice/FinalInvoice.js
+++ components/app/Invoice/FinalInvoice.js
@@ -161,7 +161,7 @@ function FinalInvoice(props) {
     let month = date.month();
   
     // If the month is less than or equal to 3, then the financial year is the previous year.
-    if (month <= 3) {
+    if (month <= 2) {
       return (currentYear - 1).toString().slice(2,4);
     }
   
@@ -175,7 +175,7 @@ function FinalInvoice(props) {
       const invoicePrefixConst = '0000000';
       const invoiceNumberParts = {};
       let finalInvoiceNumber = '';
-      const currentYear = getFinancialYear(moment());
+      const currentYear = getFinancialYear(moment()); 
       if (result && result.status) {
         invoiceNumberParts.year = result.data.slice(0, 2);
         invoiceNumberParts.currentNumber = result.data.slice(
